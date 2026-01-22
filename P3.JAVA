0610cc03f45e53b994d98cfabea13b447b7f7cd0
unnati-07775 <240390107047@saffrony.ac.in>
2026-01-22 21:51:39 +0530

    Add files via upload

---
+++ P3.JAVA
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class P3 {
+    public static void main(String args[]) {
+        System.out.print("Enter character: ");
+        Scanner sc = new Scanner(System.in);
+
+        String ch = sc.next().toLowerCase();  // convert to lowercase
+
+        if (ch.equals("a") || ch.equals("e") || ch.equals("i") ||
+            ch.equals("o") || ch.equals("u")) {
+            System.out.println("Entered Character is a Vowel");
+        } else {
+            System.out.println("Entered Character is a Consonant");
+        }
+    }
+}
